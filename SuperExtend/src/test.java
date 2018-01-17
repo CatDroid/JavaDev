61a735fc869d1da2c4f5df693e812bb8a42b3a4a
Hanlon <1198432354@qq.com>
2018-01-17 20:41:45 +0800

    Java  泛型编程 <? extends T> 和 <? super T> extends T 上界 代表里面的元素都是 T类 or T的派生类 正如 Derice d = new Base() 是错误的  extends T 不能 add(T t) 派生类到  参数需要 显式 强制转换 super T 下限  代表里面的元素都是 T类 or T的父类  正如 Base d = new Derive() 是可以的 所以 可以往里面 add(T t) T类或T派生类实例 但是不可以是Object 或者T的父类 因为super T限制里面实例最低应该是T类

---
+++ SuperExtend/src/test.java
@@ -0,0 +1,102 @@
+
+public class test {
+
+	static class Plate<T>{
+	    private T item;
+	    public Plate(T t){item=t;}
+	    public void set(T t){item=t;}
+	    public T get(){return item;}
+	}
+	
+	//Lev 1
+	static class Food{}
+
+	//Lev 2
+	static class Fruit extends Food{}
+	static class Meat extends Food{}
+
+	//Lev 3
+	static class Apple extends Fruit{}
+	static class Banana extends Fruit{}
+	static class Pork extends Meat{}
+	static class Beef extends Meat{}
+
+	//Lev 4
+	static class RedApple extends Apple{}
+	static class GreenApple extends Apple{}
+	
+	
+	public static void main(String[] arg) {
+		
+		{
+			Plate<? extends Fruit> p= new Plate<Apple>(new Apple());
+
+			//不能存入任何元素
+			//p.set(new Fruit());    //Error
+			//p.set(new Apple());    //Error
+			// 	
+		 
+			
+			// 读取出来的东西只能存放在Fruit或它的基类里  , 具体是什么类型不知道 可能是Fruit Apple Banana RedApple GreenApple
+			Fruit newFruit1 = p.get();
+			// Object newFruit2 = p.get();
+			// Apple  newFruit3 = p.get();    //Error
+			
+			
+			/*
+			   Number:
+			   		Java语言为每一个内置数据类型提供了对应的包装类
+			   		 所有的包装类(Integer、Long、Byte、Double、Float、Short)都是抽象类Number的子类
+
+			   
+			   	List<? extends E>
+			   		表示该list集合中存放的都是E的子类型(包括E自身)，由于E的子类型可能有很多，
+			   		 但是我们存放元素时，实际上只能存放其中的一种子类型
+			   		(这是为了泛型安全, 因为其会在编译期间生成桥接方法<Bridge Methods>该方法中会出现强制转换, 若出现多种子类型, 则会强制转换失败）
+			   		
+			   		List<? extends Number> list=new ArrayList<Number>();
+        			list.add(4.0);	// 编译错误
+        			list.add(3);	// 编译错误			 
+			   					 
+			   
+		 		List<? extends E>不能添加元素，但是由于其中的元素都有一个共性
+		 			--有共同的父类，
+		 			因此我们在获取元素时可以将他们统一强制转换为E类型，我们称之为GET原则
+		 			
+		 			Apple f = new Fruit(); // 不能   --> 所以  List<? extends Fruit> .add( new Apple ) 错误
+		 			
+		 			get是可以  容器里面可能是Apple Banana 可以get到给 Fruit 
+		 			
+		 		List<? super E>其list中存放的都是E的父类型元素(包括E) 
+		 			我们在向其添加元素时，只能向其添加E的子类型元素(包括E类型)
+		 			这样在编译期间将其强制转换为E类型时是类型安全的，因此可以添加元素
+		 		
+		 		也就是 List<? super Fruit> .add( new Apple ) 是正常的    Fruit f = new Apple();
+			 */
+			
+		}
+		
+		{
+			//Plate<? super Fruit> p = new Plate<Apple>(new Apple()); // Error 
+			Plate<? super Fruit> p1 = new Plate<Object>(new Object());
+			Plate<? super Fruit> p2 = new Plate<Fruit>(new Fruit());
+		}
+		
+		{
+			Plate<? super Fruit> p = new Plate<Fruit>(new Fruit());
+
+			//存入元素正常
+			p.set(new Fruit()); 
+			p.set(new Apple()); // Apple --> Fruit 不用显式强制转换  
+			// p.set(new Object() ); // Error  Object --> Fruit  !!!  因为super T限制里 面实例最低应该是T类
+
+			//读取出来的东西只能存放在Object类里。
+			//Apple newFruit3=(Apple) p.get();    //Error
+			//Fruit newFruit1=  p.get();    //Error
+			//Object newFruit2=p.get();
+		}
+		
+		
+		
+	}
+}
